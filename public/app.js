22d7d083fc74554d8a59d58cb8185a5e5b5ad51e
iancorbett <iancorbett1324@gmail.com>
2025-09-19 15:14:03 -0700

    begin app.js, using shorthand trick i learned as well as initializing PIN to empty string

---
+++ public/app.js
@@ -0,0 +1,3 @@
+const $ = sel => document.querySelector(sel); //allows shorthand
+const pinInput = $('#pin'); //grab pin input element from html page
+let PIN = ''; //initialize PIN to an empty string
