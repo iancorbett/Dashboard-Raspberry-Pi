f722a721336cc48b74e8272ee8353ccabe678e7e
iancorbett <iancorbett1324@gmail.com>
2025-09-19 15:17:37 -0700

    add hdr function, going to try to create functionality for the clock now too

--- public/app.js
+++ public/app.js
@@ -1,3 +1,5 @@
 const $ = sel => document.querySelector(sel); //allows shorthand
 const pinInput = $('#pin'); //grab pin input element from html page
 let PIN = ''; //initialize PIN to an empty string
+
+function hdr() { return PIN ? { 'x-dash-pin': PIN } : {}; } //if a PIN has been set, this builds a header object, if no PIN, it just returns {} (an empty header set)
