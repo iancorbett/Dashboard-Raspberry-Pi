cc89902b08146da8391e060283ec326b9418cfdf
iancorbett <iancorbett1324@gmail.com>
2025-09-19 22:54:17 -0700

    adding function to update clock, after research, this is possible in vanilla js and does not requre the use of dayjs

--- public/app.js
+++ public/app.js
@@ -3,3 +3,8 @@ const pinInput = $('#pin'); //grab pin input element from html page
 let PIN = ''; //initialize PIN to an empty string
 
 function hdr() { return PIN ? { 'x-dash-pin': PIN } : {}; } //if a PIN has been set, this builds a header object, if no PIN, it just returns {} (an empty header set)
+
+function tickClock() {
+    const d = new Date(); //find current date
+    $('#clock').textContent = d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); // find local time and return in proper format
+  }
