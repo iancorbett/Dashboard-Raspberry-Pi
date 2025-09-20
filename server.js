6cd77e600a627847b464e1e56dab08f43322fc75
iancorbett <iancorbett1324@gmail.com>
2025-09-19 23:30:36 -0700

    add server code to connect our backend to our frontend files, it will find where the current file lives

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ app.use(express.json());
 
 app.get('/health', (req, res) => res.json({ status: 'ok', uptime: process.uptime() }));
 app.get('/version', (req, res) => res.json({ sha: GIT_SHA }));
+
+const __filename = fileURLToPath(import.meta.url); // full path to current file
+const __dirname  = path.dirname(__filename); // folder path (no filename)
+app.use(express.static(path.join(__dirname, 'public'))); //joins the current folder (__dirname) with public/
